f4e21ccd692a18cb810e35f92932b3f51ce38131
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2023-02-15 17:10:13 +0100

    authetification à vérifier !

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,30 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import HomePage from "./HomePage";
+import Login from "./Login";
 import BenevoleContenu from './components/BenevoleContenu';
+import PrivateRoute from "./auth/PrivateRoute";
+import { PrivateRouteProps } from "./auth/PrivateRoute";
+import { AuthContext } from "./auth/AuthContext";
+import { useContext } from "react";
+
+const defaultProtectedRouteProps: Omit<PrivateRouteProps, 'outlet'> = {
+  isAuthenticated: useContext(AuthContext).isAuthenticated,
+  authenticationPath: "/login",
+};
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/benevoles" element={<BenevoleContenu />} />
-        <Route path="/jeux" element={<div>Jeux</div>} />
-        <Route path="/affectations" element={<div>Affectations</div>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/benevoles" element={<PrivateRoute {...defaultProtectedRouteProps} outlet={<BenevoleContenu />} /> } />
+          <Route path="/jeux" element={<PrivateRoute {...defaultProtectedRouteProps} outlet={<div>Jeux</div>} /> } />
+          <Route path="/affectations" element={<PrivateRoute {...defaultProtectedRouteProps} outlet={<div>Affectations</div>} /> } />
+        </Routes>
       </div>
       <Footer />
     </div>
