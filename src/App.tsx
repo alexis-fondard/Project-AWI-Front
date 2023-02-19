e0f6f0c1ff44f601727884666933fc5d3ce2960b
Maxime Stefani <max341@free.fr>
2023-02-19 22:24:30 +0100

    add affectationsZone and affectationsCreneaux routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ import Header from './Header';
 import Footer from './Footer';
 import HomePage from "./HomePage";
 import Login from "./Login";
-import BenevoleContenu from './components/BenevoleContenu';
+import BenevoleContenu from './components/Benevoles/BenevoleContenu';
 import PrivateRoute from "./auth/PrivateRoute";
 import { PrivateRouteProps } from "./auth/PrivateRoute";
 import { AuthContext } from "./auth/AuthContext";
 import { useContext } from "react";
+import AffectationZoneContenu from "./components/AffectationsZones/AffectationZoneContenu";
+import AffectationCreneauContenu from "./components/AffectationsCreneau/AffectationsCreneauContenu";
 
 const defaultProtectedRouteProps: Omit<PrivateRouteProps, 'outlet'> = {
   isAuthenticated: useContext(AuthContext).isAuthenticated,
@@ -30,6 +32,8 @@ function App() {
           <Route path="/benevoles" element={<PrivateRoute {...defaultProtectedRouteProps} outlet={<BenevoleContenu />} /> } />
           <Route path="/jeux" element={<PrivateRoute {...defaultProtectedRouteProps} outlet={<div>Jeux</div>} /> } />
           <Route path="/affectations" element={<PrivateRoute {...defaultProtectedRouteProps} outlet={<div>Affectations</div>} /> } />
+          <Route path="/affectationsZone" element={<AffectationZoneContenu/>}/>
+          <Route path="/affectationsCreneau" element={<AffectationCreneauContenu/>}/>
         </Routes>
       </div>
       <Footer />
